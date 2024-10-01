38d877d8c59c4bd324d0261b1f4012c5c671ac04
Michelle Hon <michellecyhon@gmail.com>
2024-09-30 17:08:25 -0700

    Added methods and method specification for BouquetCustomizationApp class

--- src/main/ui/BouquetCustomizationApp.java
+++ src/main/ui/BouquetCustomizationApp.java
@@ -1,5 +1,47 @@
 package ui;
 
+import java.util.List;
+
+// Bouquet customization applicaation
 public class BouquetCustomizationApp {
 
+    // EFFECTS: runs the customization application
+    public BouquetCustomizationApp(){
+        //stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user input
+    private void runCustomization(){
+        //stub
+    }
+
+    // EFFECTS: displays menu of options to user
+    private void displayMenu(){
+        //stub
+    }
+
+    // MOFIDIES: this
+    // EFFECTS: adds a flower to the collection of requirements 
+    // for the bouquet
+    private void addFlower(String flowerName){
+        //stub
+    }
+
+    // MOFIDIES: this
+    // EFFECTS: removes a flower from the collection of requirements 
+    // for the bouquet
+    private void removeFlower(String flowerName){
+        //stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: returns the list of requirements 
+    // the user has inputted for the bouquet, 
+    // the list returned could be empty (it is never null)
+    public List<String> bouquetRequirements() {
+        return null;  // stub
+    }
+
+    //not sure how popular bouquet will work
 }
