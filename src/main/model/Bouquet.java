9a96ccf826cd61b34991527d5bfbf349699496b5
Michelle Hon <michellecyhon@gmail.com>
2024-09-30 17:08:45 -0700

    Added methods and method specification for Bouquet class

--- src/main/model/Bouquet.java
+++ src/main/model/Bouquet.java
@@ -1,9 +1,20 @@
 package model;
 
+// Represents a bouquet having a customer name and price (in dollars)
 public class Bouquet {
-    int other;
+    private String name;
+    private double price;
 
-    public Sample() {
-        other = 4;
+    // EFFECTS: name is set to customerName, price is set to zero
+    public Bouquet(String customerName, double bouquetPrice) {
+        //stub
+    }
+
+    public String getName(){
+        return null; //stub
+    }
+
+    public double getPrice(){
+        return null; //stub
     }
 }
