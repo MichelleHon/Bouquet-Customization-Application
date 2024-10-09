2b83f49b4f01ccb7b9661fc1c10ed4f105b5b5fe
MichelleHon <michellecyhon@gmail.com>
2024-10-08 23:39:21 -0700

    Added all user story methods and specifications

--- src/main/model/Bouquet.java
+++ src/main/model/Bouquet.java
@@ -1,9 +1,13 @@
 package model;
 
+import java.util.List;
+import java.util.ArrayList;
+
 // Represents a bouquet having a customer name and price (in dollars)
 public class Bouquet {
-    private String name;
-    private double price;
+    private String name;  // customer name
+    private double price; // current price of bouquet
+    private List<String> flowerNames = new ArrayList<>(); // flower names for bouquet requirements
 
     // EFFECTS: name is set to customerName, price is set to zero
     public Bouquet(String customerName, double bouquetPrice) {
@@ -11,10 +15,48 @@ public class Bouquet {
     }
 
     public String getName(){
-        return null; //stub
+        return ""; 
     }
 
     public double getPrice(){
-        return null; //stub
+        return 0; 
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds a flower to the collection of requirements for the bouquet
+    public void aFlower(String flower){
+        //stub
+    }
+
+    // MOFIDIES: this
+    // EFFECTS: removes a flower from the collection of requirements 
+    // for the bouquet
+    public void rFlower(String flower){
+        //stub
+    }
+
+    // EFFECTS: returns the list of requirements 
+    // the user has inputted for the bouquet, 
+    // the list displayed could be empty (it is never null)
+    public List<String> bouquetRequirements() {
+        return new ArrayList<>();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: add all flowers for popular bouquet 1
+    public void popularBouquetOne() {
+        //stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: add all flowers for popular bouquet 2
+    public void popularBouquetTwo() {
+        //stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: add all flowers for popular bouquet 3
+    public void popularBouquetThree() {
+        //stub
     }
 }
