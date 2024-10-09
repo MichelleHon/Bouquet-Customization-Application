b3e45c5974ceb05c6ceb6723db039de1f2736c9b
MichelleHon <michellecyhon@gmail.com>
2024-10-09 11:09:37 -0700

    Added implementation for Bouquet class

--- src/main/model/Bouquet.java
+++ src/main/model/Bouquet.java
@@ -1,62 +1,89 @@
 package model;
 
-import java.util.List;
 import java.util.ArrayList;
 
-// Represents a bouquet having a customer name and price (in dollars)
+// Represents a bouquet having a customer name, price (in dollars) and the list of requirements
 public class Bouquet {
     private String name;  // customer name
     private double price; // current price of bouquet
-    private List<String> flowerNames = new ArrayList<>(); // flower names for bouquet requirements
+    private ArrayList<String> flowerNames = new ArrayList<>(); // flower names for bouquet requirements
 
     // EFFECTS: name is set to customerName, price is set to zero
     public Bouquet(String customerName, double bouquetPrice) {
-        //stub
+        this.name = customerName;
+        this.price = bouquetPrice;
     }
 
     public String getName(){
-        return ""; 
+        return name; 
     }
 
     public double getPrice(){
-        return 0; 
+        return price; 
     }
 
     // MODIFIES: this
     // EFFECTS: adds a flower to the collection of requirements for the bouquet
     public void aFlower(String flower){
-        //stub
+        flowerNames.add(flower);
     }
 
     // MOFIDIES: this
     // EFFECTS: removes a flower from the collection of requirements 
     // for the bouquet
     public void rFlower(String flower){
-        //stub
+        if (flowerNames.contains(flower)) {
+            flowerNames.remove(flower);
+        } else {
+            System.out.println("Flower not in bouquet"); //check if i cant add these
+        }
     }
 
     // EFFECTS: returns the list of requirements 
     // the user has inputted for the bouquet, 
     // the list displayed could be empty (it is never null)
-    public List<String> bouquetRequirements() {
-        return new ArrayList<>();
+    public ArrayList<String> bouquetRequirements() {
+        return flowerNames;
     }
 
     // MODIFIES: this
     // EFFECTS: add all flowers for popular bouquet 1
     public void popularBouquetOne() {
-        //stub
+        for(int i = 0; i < 12; i++) {
+            flowerNames.add("Rose");
+        }
+        flowerNames.add("Greens");
     }
 
     // MODIFIES: this
     // EFFECTS: add all flowers for popular bouquet 2
     public void popularBouquetTwo() {
-        //stub
+        for(int i = 0; i < 3; i++) {
+            flowerNames.add("Sunflower");
+            flowerNames.add("Daisy");
+            flowerNames.add("Lily");
+            flowerNames.add("Snowdrop");
+            flowerNames.add("Greens");
+        }
+        flowerNames.add("Sunflower");
+        flowerNames.add("Daisy");
+        flowerNames.add("Daisy");
     }
 
     // MODIFIES: this
     // EFFECTS: add all flowers for popular bouquet 3
     public void popularBouquetThree() {
-        //stub
+        for(int i = 0; i < 3; i++) {
+            flowerNames.add("Tulip");
+            flowerNames.add("Lavender");
+            flowerNames.add("Lilac");
+            flowerNames.add("Dahlias");
+            flowerNames.add("Bellflower");
+            flowerNames.add("Rosemary");
+        }
+        flowerNames.add("Tulip");
+        flowerNames.add("Lavender");
+        flowerNames.add("Greens");
+        flowerNames.add("Greens");
     }
 }
