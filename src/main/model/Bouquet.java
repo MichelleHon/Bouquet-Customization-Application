520d3eea7f005373dd6aec83126ef77fd914702a
MichelleHon <michellecyhon@gmail.com>
2024-10-21 21:56:31 -0700

    Added methods and methods specification to fit phase one requriements

--- src/main/model/Bouquet.java
+++ src/main/model/Bouquet.java
@@ -2,84 +2,68 @@ package model;
 
 import java.util.ArrayList;
 
-// Represents a bouquet having a customer name, price (in dollars) and the list of requirements
+// Represents a bouquet having a customer name, the list of requirements and the price (in dollars)
 public class Bouquet {
-    private String name;  // customer name
+    private String name; // customer name
     private double price; // current price of bouquet
-    private ArrayList<String> flowerNames = new ArrayList<>(); // flower names for bouquet requirements
+    private ArrayList<Flower> flowerNames; // list of flower names for bouquet requirements
 
-    // EFFECTS: name is set to customerName, price is set to zero
-    public Bouquet(String customerName, double bouquetPrice) {
+    // EFFECTS: name is set to customerName, input of flowerNames is set to
+    // flowerNames for this Bouquet
+    // and price is set to zero
+    public Bouquet(String customerName, ArrayList<Flower> flowerNames, double bouquetPrice) {
         this.name = customerName;
         this.price = bouquetPrice;
+        this.flowerNames = flowerNames;
     }
 
     public String getName() {
-        return name; 
+        return name;
     }
 
     public double getPrice() {
-        return price; 
+        return price;
     }
 
     // MODIFIES: this
     // EFFECTS: adds a flower to the collection of requirements for the bouquet
-    public void flowerAdd(String flower) {
+    public void flowerAdd(Flower flower) {
         flowerNames.add(flower);
     }
 
     // MOFIDIES: this
     // EFFECTS: removes a flower from the collection of requirements for the bouquet
-    public void flowerRemove(String flower) {
+    public void flowerRemove(Flower flower) {
         flowerNames.remove(flower);
-        
+
     }
 
-    // EFFECTS: returns the list of requirements 
-    // the user has inputted for the bouquet, 
+    // EFFECTS: returns the list of requirements
+    // the user has inputted for the bouquet,
     // the list displayed could be empty (it is never null)
-    public ArrayList<String> bouquetRequirements() {
+    public ArrayList<Flower> getBouquetRequirements() {
         return flowerNames;
     }
 
-    // MODIFIES: this
-    // EFFECTS: add all flowers for popular bouquet 1
-    public void popularBouquetOne() {
-        for (int i = 0; i < 12; i++) {
-            flowerNames.add("Rose");
-        }
-        flowerNames.add("Greens");
+    // EFFECTS: add the requirement of PopularBouquetOne from PopularBouquets class
+    // to list of requirements in Bouquet
+    // this is done so users can add and or remove flowers for further
+    // customization of their bouquet
+    public void addPopBouquetOne() {
     }
 
-    // MODIFIES: this
-    // EFFECTS: add all flowers for popular bouquet 2
-    public void popularBouquetTwo() {
-        for (int i = 0; i < 3; i++) {
-            flowerNames.add("Sunflower");
-            flowerNames.add("Daisy");
-            flowerNames.add("Lily");
-            flowerNames.add("Snowdrop");
-            flowerNames.add("Greens");
-        }
-        flowerNames.add("Sunflower");
-        flowerNames.add("Daisy");
-        flowerNames.add("Daisy");
+    // EFFECTS: add the requirement of PopularBouquetTwo from PopularBouquets class
+    // to list of requirements in Bouquet
+    // this is done so users can add and or remove flowers for further
+    // customization of their bouquet
+    public void addPopBouquetTwo() {
     }
 
-    // MODIFIES: this
-    // EFFECTS: add all flowers for popular bouquet 3
-    public void popularBouquetThree() {
-        for (int i = 0; i < 3; i++) {
-            flowerNames.add("Tulip");
-            flowerNames.add("Lavender");
-            flowerNames.add("Lilac");
-            flowerNames.add("Dahlias");
-            flowerNames.add("Bellflower");
-            flowerNames.add("Rosemary");
-        }
-        flowerNames.add("Tulip");
-        flowerNames.add("Lavender");
-        flowerNames.add("Greens");
-        flowerNames.add("Greens");
+    // EFFECTS: add the requirement of PopularBouquetThree from PopularBouquets
+    // class
+    // to list of requirements in Bouquet
+    // this is done so users can add and or remove flowers for further
+    // customization of their bouquet
+    public void addPopBouquetThree() {
     }
 }
