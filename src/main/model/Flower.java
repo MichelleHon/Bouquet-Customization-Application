cf909d4dc0374d20e62d93e331c8075209d32b56
MichelleHon <michellecyhon@gmail.com>
2024-10-21 22:00:02 -0700

    Created an instance of Flower and added methods and methods specification to fit phase one requirements

---
+++ src/main/model/Flower.java
@@ -0,0 +1,22 @@
+package model;
+
+// Represents a flower or greenary that would be added to the bouquet
+public class Flower {
+    private String flowerName; // flower name
+
+    // EFFECTS: input of flowerName is set to flowerName for this Flower
+    public Flower(String flowerName) {
+        //stub
+    }
+
+    public String getFlowerName() {
+        return ""; //stub
+    }
+
+    // EFFECTS: returns a string representation of Flower
+    // code source from TellerApp (with exceptions)
+    @Override
+    public String toString() {
+        return ""; //stub
+    }
+}
