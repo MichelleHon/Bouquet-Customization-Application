5b8474a08509ea2d0dcc16887e8ad2763266dadd
MichelleHon <michellecyhon@gmail.com>
2024-10-24 10:05:48 -0700

    Modified to accomodate phase 2

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,7 +1,13 @@
 package ui;
 
+import java.io.FileNotFoundException;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        new BouquetCustomizationApp();
+    public static void main(String[] args) {
+        try {
+            new BouquetCustomizationApp();
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to run application: file not found");
+        }
     }
 }
