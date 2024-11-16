cac0deee0aec101e05ea5183bed15bfdd9c5c05d
Michelle Hon <michellecyhon@gmail.com>
2024-11-16 12:26:27 -0800

    Added comment for what this class does based on phase 2 feedback

--- src/main/persistence/Writable.java
+++ src/main/persistence/Writable.java
@@ -2,6 +2,9 @@ package persistence;
 
 import org.json.JSONObject;
 
+// An interface for objects that can be serialized into a 
+// JSON representation
+
 // code source from JsonSerializationDemo
 public interface Writable {
     // EFFECTS: returns this as JSON object
